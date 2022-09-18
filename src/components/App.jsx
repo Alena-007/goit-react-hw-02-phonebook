00c37bc5806dc42a5cf560e080111e81688636d6
Alena-007 <alena.tv@ukr.net>
2022-09-18 13:45:20 +0300

    add updates steps 1-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -59,6 +59,9 @@ export class App extends Component {
             <input
               type="text"
               name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
               value={name}
               onChange={this.changeName}
             />
@@ -66,8 +69,11 @@ export class App extends Component {
           <label>
             Number
             <input
-              type="text"
+              type="tel"
               name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
               value={number}
               onChange={this.changeNumber}
             />
